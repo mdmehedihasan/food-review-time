bf2386fd1da2bcd68af4ab930c15b401f4a971ed
mehedi hasan <mehedicsebu@gmail.com>
2022-04-05 02:36:03 +0600

    blogs section desing start

--- src/Component/Blogs/Blogs.js
+++ src/Component/Blogs/Blogs.js
@@ -2,8 +2,20 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <div>
-            <p>This is blog</p>
+        <div className='flex px-16'>
+            <div className='flex-1'>
+                <h1 className='text-xl font-bold my-6 bg-orange-500 text-white mx-3 py-3'>
+                    What is the purpose of Context Api?
+                </h1>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex, possimus. Ex asperiores fuga officiis consequuntur explicabo ratione id amet dolores obcaecati est. Animi, enim et impedit illum possimus ab vitae id, fuga voluptatem excepturi molestiae autem corrupti veniam libero iusto!</p>
+            </div>
+            <div className='flex-1'>
+                <h1 className='text-xl font-bold my-6 bg-orange-500 text-white mx-3 py-3 '>
+                    What is the purpose of Context Api?
+                </h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam est rerum, et obcaecati impedit at! Voluptatibus nulla blanditiis eligendi eveniet voluptate, repudiandae fugit praesentium unde et sit iusto magni velit ut totam quo! Ab accusantium ea expedita, distinctio rerum quidem?</p>
+            </div>
+
         </div>
     );
 };
