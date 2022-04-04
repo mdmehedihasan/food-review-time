b8d30a5b9d6ab75eaa539849fdaba17d881dcd15
mehedi hasan <mehedicsebu@gmail.com>
2022-04-05 00:24:24 +0600

    Clients review page done

--- src/Component/Reviews/Reviews.js
+++ src/Component/Reviews/Reviews.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import useReviewes from '../../Hooks/useReviews';
+import Review from '../Review/Review';
 
 const Reviews = () => {
+    const [reviews, setReviews] = useReviewes();
     return (
         <div>
-            <p>Reviews pagae</p>
+            <h1 className='text-3xl font-bold my-6'>
+                Client,s Review
+            </h1>
+            <div className='grid grid-cols-3 grid-rows-2 gap-5 px-5'>
+
+                {
+                    reviews.map(review => <Review
+                        key={review._id}
+                        review={review}
+                    ></Review>)
+                }
+            </div>
         </div>
     );
 };
