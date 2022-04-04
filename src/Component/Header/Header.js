ff7cdb285230631a34a3416969d761312ec30d8e
mehedi hasan <mehedicsebu@gmail.com>
2022-04-04 19:48:32 +0600

    Header design done

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -3,13 +3,17 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-
-        <nav>
-            <Link to="/home">Home </Link>
-            <Link to="/reviews">Reviews</Link>
-            <Link to="/dashboard">DashBoard</Link>
-            <Link to="/blogs">Blogs</Link>
-            <Link to="/about">About</Link>
+        <nav className='bg-amber-600  font-bold text-white py-5'>
+            <div className='mb-5'>
+                <h2 className='text-3xl'>Food Review Time</h2>
+            </div>
+            <div>
+                <Link className='mr-5 px-2 border-2 rounded-lg' to="/home">HOME</Link>
+                <Link className='mr-5 px-2 border-2 rounded-lg ' to="/reviews">REVIEWS</Link>
+                <Link className='mr-5 px-2 border-2 rounded-lg' to="/dashboard">DASHBOARD</Link>
+                <Link className='mr-5 px-2 border-2 rounded-lg' to="/blogs">BLOGS</Link>
+                <Link className='mr-5 px-2 border-2 rounded-lg' to="/about">ABOUT</Link>
+            </div>
         </nav>
     );
 };
