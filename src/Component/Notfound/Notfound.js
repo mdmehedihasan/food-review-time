4c796985ffda72dcbc490a068edff8c36aef840c
mehedi hasan <mehedicsebu@gmail.com>
2022-04-05 05:04:13 +0600

    404 page updated

--- src/Component/Notfound/Notfound.js
+++ src/Component/Notfound/Notfound.js
@@ -2,8 +2,22 @@ import React from 'react';
 
 const Notfound = () => {
     return (
-        <div>
-            <p>Not found page</p>
+        <div className='flex'>
+            <div className=' flex flex-1 flex-col justify-center items-center p-6'>
+                <h2 className='text-3xl font-bold all uppercase p-6'>The page you search is not found. Please check the url and try again.</h2>
+                <h3 className='text-xl font-bold'>What You Do Now?</h3>
+                <div className='mt-5'>
+                    <button className='border-2 mr-3 px-3 py-1 bg-orange-400 rounded-lg text-white'><a href="/home">Go Home</a></button>
+
+                    <button className='border-2  px-3 py-1 bg-orange-400 rounded-lg text-white'><a href="/about">Contact Us</a></button>
+
+
+                </div>
+            </div>
+            <div className='flex-1'>
+                <img src="images/404page.jpg" alt="" />
+            </div>
+
         </div>
     );
 };
