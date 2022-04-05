143324d5d87601c6533d6632415a85d1d8bcc929
mehedi hasan <mehedicsebu@gmail.com>
2022-04-05 14:54:34 +0600

    address edited

--- src/Component/About/About.js
+++ src/Component/About/About.js
@@ -2,9 +2,15 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <p>This is about</p>
+
+        <div className='my-5'>
+            <h1 className='text-3xl font-bold py-3'>We Provide Fresh Food</h1>
+            <p className='text-xl font-bold'>We are always try to give fresh item and take the rsponsibility</p>
+            <h3 className='text-2xl font-semibold'>Our Address:</h3>
+            <p className='text-xl'> House No.84, Road No, 7/A, Satmasjid Road, Dhaka 1205.</p>
+
         </div>
+
     );
 };
 
--- src/Component/Blogs/Blogs.js
+++ src/Component/Blogs/Blogs.js
@@ -11,18 +11,26 @@ const Blogs = () => {
                     <li className='bg-slate-200 p-2'>The React Context Api is a way for effectively produce global variables that can be passed around</li>
                     <li className='bg-slate-200 p-2'>This is alternative to "props drilling" or moving props from grandparent to child to parent</li>
                     <li className='bg-slate-200 p-2'>
-                        Context is also touted as an easier, lighter approach to state management using Redux
+                        Context is also touted as an easier, lighter approach to state management using Redux.
                     </li>
+
                 </p>
             </div>
             <div className='flex-1'>
                 <h1 className='text-xl font-bold my-6 bg-orange-500 text-white mx-3 py-3 '>
-                    What is the purpose of Context Api?
+                    What is Semantic Tag and How it works?
                 </h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam est rerum, et obcaecati impedit at! Voluptatibus nulla blanditiis eligendi eveniet voluptate, repudiandae fugit praesentium unde et sit iusto magni velit ut totam quo! Ab accusantium ea expedita, distinctio rerum quidem?</p>
+                <p className='text-left mx-3'>
+                    <li className='bg-slate-200 p-2'>Semantic HTML elements are those that clearly describe their meaning in a human and machine readable way.</li>
+                    <li className='bg-slate-200 p-2'>It is much easier to read,so it has greater accessibility</li>
+                    <li className='bg-slate-200 p-2'>
+                        Elements such as "header", "footer" and "footer" are considered semantic beacuse they accurately describe the purpose of the element and the type of content.
+                    </li>
+
+                </p>
             </div>
 
-        </div>
+        </div >
     );
 };
 
