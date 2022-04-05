b8af2ef3f0b8955cdc16bad3a4a12d4a17577dde
mehedi hasan <mehedicsebu@gmail.com>
2022-04-05 12:18:40 +0600

    the purpose of context api added

--- src/Component/Blogs/Blogs.js
+++ src/Component/Blogs/Blogs.js
@@ -7,7 +7,13 @@ const Blogs = () => {
                 <h1 className='text-xl font-bold my-6 bg-orange-500 text-white mx-3 py-3'>
                     What is the purpose of Context Api?
                 </h1>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex, possimus. Ex asperiores fuga officiis consequuntur explicabo ratione id amet dolores obcaecati est. Animi, enim et impedit illum possimus ab vitae id, fuga voluptatem excepturi molestiae autem corrupti veniam libero iusto!</p>
+                <p className='text-left mx-3'>
+                    <li className='bg-slate-200 p-2'>The React Context Api is a way for effectively produce global variables that can be passed around</li>
+                    <li className='bg-slate-200 p-2'>This is alternative to "props drilling" or moving props from grandparent to child to parent</li>
+                    <li className='bg-slate-200 p-2'>
+                        Context is also touted as an easier, lighter approach to state management using Redux
+                    </li>
+                </p>
             </div>
             <div className='flex-1'>
                 <h1 className='text-xl font-bold my-6 bg-orange-500 text-white mx-3 py-3 '>
